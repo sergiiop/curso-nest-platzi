a904cead708038e4f207ff460ceda57fd145237a
Sergio Perez <sergio-luis98@hotmail.com>
2023-01-13 17:57:39 -0500

    updating

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Query, Param, Post } from '@nestjs/common'
+import {
+  Controller,
+  Get,
+  Query,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+  HttpStatus,
+  HttpCode,
+} from '@nestjs/common'
 
 @Controller('products')
 export class ProductsController {
@@ -8,23 +19,44 @@ export class ProductsController {
     @Query('offset') offset: number = 0,
     @Query('brand') brand: string,
   ) {
-    return `product: limit => ${limit}, offset => ${offset} => ${brand}`
+    return {
+      message: `product: limit => ${limit}, offset => ${offset} => ${brand}`,
+    }
   }
 
   @Get('filter')
   getProductFilter() {
-    return `Filter`
+    return {
+      message: 'Filter',
+    }
   }
 
   @Get(':id')
+  @HttpCode(HttpStatus.ACCEPTED)
   getProduct(@Param('id') id: string) {
-    return `product ${id}`
+    return { message: `product ${id}` }
   }
 
   @Post()
-  create() {
+  create(@Body() payload: any) {
     return {
       message: 'Accion para crear',
+      payload,
+    }
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any) {
+    return {
+      id,
+      payload,
+    }
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return {
+      id,
     }
   }
 }
